fe6e3a27e4d2e6aa3c7f758104d90fca4e8bbd0e
guojing <1026546013@qq.com>
2020-12-11 20:09:12 +0800

    个人中心模块完成

--- schema/user.js
+++ schema/user.js
@@ -4,6 +4,12 @@ const joi = require('@hapi/joi')
 const username = joi.string().alphanum().min(1).max(10).required()
 // 密码的验证规则
 const password = joi.string().pattern(/^[\S]{6,12}$/).required()
+// 定义 id, nickname, emial 的验证规则
+const id = joi.number().integer().min(1).required()
+const nickname = joi.string().required()
+const email = joi.string().email().required()
+//定义头像验证规则
+const avatar = joi.string().dataUri().required()
 
 // 注册和登录表单的验证规则对象
 exports.reg_login_schema = {
@@ -12,4 +18,32 @@ exports.reg_login_schema = {
         username,
         password,
     },
+}
+
+// 验证规则对象 - 更新用户基本信息
+exports.update_userinfo_schema  = {
+    body:{
+        id,
+        nickname,
+        email,
+    },
+}
+
+// 验证规则对象 - 重置密码
+exports.update_password_schema  = {
+    body:{
+        // 使用 password 这个规则，验证 req.body.oldPwd 的值
+        oldPwd:password,
+        // 1. joi.ref('oldPwd') 表示 newPwd 的值必须和 oldPwd 的值保持一致
+        // 2. joi.not(joi.ref('oldPwd')) 表示 newPwd 的值不能等于 oldPwd 的值
+        // 3. .concat() 用于合并 joi.not(joi.ref('oldPwd')) 和 password 这两条验证规则
+        newPwd:joi.not(joi.ref('oldPwd')).concat(password),
+    }
+}
+
+// 验证规则对象 - 更新头像
+exports.update_avatar_schema  = {
+    body:{
+        avatar,
+    }
 }
